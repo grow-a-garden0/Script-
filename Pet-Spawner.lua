8ec695b3329d481ab0f94976bdbc3eedc2e9b72c
grow-a-garden0 <ranieltmolinas@gmail.com>
2025-06-22 09:54:14 +0800

    Update Pet-Spawner.lua

--- Pet-Spawner.lua
+++ Pet-Spawner.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/grow-a-garden0/Scriptt/refs/heads/main/2abe1a9834a2790dd3dd6689b22950d0.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GrowAGarden-1/Script/refs/heads/main/GrowGag"))()
